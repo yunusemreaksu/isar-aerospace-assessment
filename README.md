fa1abaadb707c61c745f24fbe26a64a1ac1872d0
Yunus Emre Aksu <emre_aksu@live.com>
2023-12-11 22:14:25 +0300

    add new opinions to assignment c & readme

--- README.md
+++ README.md
@@ -22,6 +22,8 @@ This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-opti
 
 These are my comments about assignment C:
 
-- I would expect each endpoint to respond in the same format. In one endpoint, the keys come in camel case, in the other, they come in pascal case.
+- I would expect each endpoint to respond in the same format. In one endpoint, the keys come in camel case, in the other, they come in pascal case. In certain cases, this may also require interfering with the structure of incoming data.
 - It would be nice if it was stated whether the returned numerical values are based on the imperial or metric system.
 - Again, it is not clear in which unit the numerical values are returned. It would be good to specify this in the API to display more accurate data.
+- If items 2 and 3 are specified in the API, the incoming data can be displayed according to the country where the user is located.
+- Incoming statusMessages may be more descriptive. For example, it can be stated more clearly whether Spectrum is currently facing a problem or not. Thus, more accurate warnings can be shown to the user.
--- app/assignment-c/client.tsx
+++ app/assignment-c/client.tsx
@@ -17,7 +17,8 @@ export default function Client() {
           <Text size={"3"} as="p">
             1. I would expect each endpoint to respond in the same format. In
             one endpoint, the keys come in camel case, in the other, they come
-            in pascal case.
+            in pascal case. In certain cases, this may also require interfering
+            with the structure of incoming data.
           </Text>
           <Text size={"3"} as="p">
             2. It would be nice if it was stated whether the returned numerical
@@ -27,6 +28,16 @@ export default function Client() {
             3. Again, it is not clear in which unit the numerical values are
             returned. It would be good to specify this in the API to display
             more accurate data.
+          </Text>
+          <Text size={"3"} as="p">
+            If items 2 and 3 are specified in the API, the incoming data can be
+            displayed according to the country where the user is located.
+          </Text>
+          <Text size={"3"} as="p">
+            4. Incoming statusMessages may be more descriptive. For example, it
+            can be stated more clearly whether Spectrum is currently facing a
+            problem or not. Thus, more accurate warnings can be shown to the
+            user.
           </Text>
         </Flex>
       </Card>
